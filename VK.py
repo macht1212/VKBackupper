214f99ff63bd341bab9d46df1fc32fec467f8225
Alexander Nazimov <nasimov.alexander@gmail.com>
2022-12-14 23:30:51 +0300

    changed files

--- VK.py
+++ VK.py
@@ -22,9 +22,10 @@ class VKPhotoBackuper:
         pprint(response.json())
 
     def get_photo_info(self):
+
         uri = 'photos.get'
         url = self.HOST + uri
-        urls_info = {}
+        urls_info = []
 
         params = {
             'access_token': self.token,
@@ -38,11 +39,21 @@ class VKPhotoBackuper:
         response = requests.get(url, params=params).json()
         response = response['response']['items']
 
-        for i, elem in enumerate(response):
-            for j, el in enumerate(response[i]['sizes']):
-                if response[i]['sizes'][j]['type'] == 'z':
-                    
-                elif response[i]['sizes'][j]['type'] == 'y':
+        photo_data = {}
 
-        pprint(response)
-        pprint(urls_info)
+        for elem in response:
+
+            res = elem['sizes']
+            max_size = 0
+            max_size_url = ''
+
+            for size in res:
+                if size['height'] >= max_size:
+                    max_size = size['height']
+                    max_size_url = size['url']
+            if f"{elem['likes']['count']}.jpg" in photo_data.keys():
+                photo_data[f"{elem['likes']['count']}+{elem['date']}.jpg"] = max_size_url
+            else:
+                photo_data[f"{elem['likes']['count']}.jpg"] = max_size_url
+
+        pprint(photo_data)
--- Yandex.py
+++ Yandex.py
@@ -5,12 +5,6 @@ from TOKEN import TOKEN_YA
 
 
 class YandexUploader:
-    """This class contain a few methods to get and push necessary information to your oun repository on claud service
-    like Yandex.Disc.
-
-    Also class connected with other class which works with VK API.
-
-    """
 
     HOST = 'https://cloud-api.yandex.net:443'
 
@@ -19,17 +13,9 @@ class YandexUploader:
         self.name = name
 
     def get_headers(self):
-        """
-        Method returns headers
-        """
         return {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.token}'}
 
     def get_upload_link(self, path_to_file):
-        """
-        Method returns response Upload link
-        :param path_to_file: name.jpg
-        :return: href for uploading
-        """
         uri = '/v1/disk/resources/upload'
         url = self.HOST + uri
         params = {'path': path_to_file}
@@ -46,13 +32,9 @@ class YandexUploader:
         }
 
         response = requests.put(url=url, headers=self.get_headers(), params=params)
+        return response.status_code
 
     def upload(self, path_to_file):
-        """
-        Method upload file to Yandex.Disk
-        :param path_to_file: name.jpg
-        :return: status of transaction: 200, 201 - all was correct, 4** - bad staff
-        """
         upload_link = self.get_upload_link(path_to_file)
         response = requests.put(upload_link, headers=self.get_headers(), data=open(self.name, 'rb'))
         print(response.status_code)
